7975e41c73e41b3895abf39dbfa24873b70dbbc7
siham <siham1554@gmail.com>
2022-01-11 12:54:37 +0300

    user

---
+++ siham-master/lib/modules/User/cubit/cubit.dart
@@ -0,0 +1,431 @@
+import 'dart:async';
+import 'dart:convert';
+import 'dart:io';
+import 'dart:typed_data';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
+import 'package:helpy_app/model/post.dart';
+
+import 'package:helpy_app/model/user_model.dart';
+import 'package:helpy_app/modules/User/cubit/states.dart';
+import 'package:helpy_app/modules/User/home_screen/user_main.dart';
+import 'package:helpy_app/modules/User/profile/profile_main.dart';
+import 'package:helpy_app/modules/customer/Chat/chats_screen.dart';
+import 'package:helpy_app/shared/componotents.dart';
+import 'package:helpy_app/shared/network.dart';
+import 'package:helpy_app/shared/shared_prefernces.dart';
+import 'package:image_picker/image_picker.dart';
+import 'package:http/http.dart' as http;
+import 'package:http_parser/http_parser.dart';
+
+import 'package:path/path.dart';
+import 'package:async/async.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:file_picker/file_picker.dart';
+
+class UserCubit extends Cubit<cons_login_Register_States> {
+  UserCubit() : super(consSignInUp_InitalState());
+  static UserCubit get(context) => BlocProvider.of(context);
+  int currentindex = 0;
+  List<Widget> screen = [
+    const UserHome(),
+    ChatsScreen(),
+    const UserProfileScreen(),
+  ];
+
+  void changeIndex(int index) {
+    currentindex = index;
+    emit(UserChangeState());
+  }
+
+  bool visable = true;
+  void changeDesign() {
+    visable = false;
+    emit(Cons_Change_Design());
+  }
+
+  void changeDesigns() {
+    visable = true;
+    emit(Cons_Change_Designs());
+  }
+
+  ///dispose controllers
+  @override
+  Future<void> close() {
+    return super.close();
+  }
+
+  String? mycity;
+  String? catSelect;
+  String? specSelect;
+  int? cat_id;
+  int? spec_id;
+
+  void changeSelectCity(val) {
+    mycity = val;
+    emit(Cons_ChangeCity_Select());
+  }
+
+  void changeSelectCategory(val) {
+    catSelect = val.title;
+    cat_id = val.id;
+    specSelect = null;
+    print(catSelect);
+    emit(Cons_Change_Cat_Select());
+  }
+
+  void changeSelectSpec(val) {
+    specSelect = val.specTitle;
+    spec_id = val.id;
+    print(specSelect);
+    emit(Cons_Change_Spec_Select());
+  }
+
+  bool type = false;
+  String myType = "Company";
+  void changeType(val) {
+    type = val;
+    if (type == false) {
+      myType = "Company";
+    } else {
+      myType = "Freelancer";
+    }
+    print(myType);
+    emit(Cons_Change_type_value());
+  }
+
+  final picker = ImagePicker();
+  final pickers = ImagePicker();
+  var pickedFile;
+  var pickedFils;
+  File? imagee;
+  Future getImageBloc(ImageSource src) async {
+    pickedFile = await picker.pickImage(source: src, imageQuality: 50);
+    if (pickedFile != null) {
+      imagee = File(pickedFile.path);
+      emit(TakeImage_State());
+      print("image selected");
+    } else {
+      print("no image selected");
+    }
+  }
+
+  deleteImageBlocLogin() {
+    imagee = null;
+    emit(DeleteImage_State());
+    print("image Deleted");
+  }
+
+  FilePickerResult? result;
+  var mediaType;
+
+  void changeMedia(bool media) {
+    if (media) {
+      mediaType = MediaType('image', 'png');
+      print(mediaType);
+    } else {
+      mediaType = MediaType('application', 'pdf');
+      print(mediaType);
+    }
+  }
+
+  int get myimagecount {
+    return result!.files.length;
+  }
+
+  deleteImageBlocList() {
+    result!.files.clear();
+    emit(DeleteImages_State());
+    print("images Deleted");
+  }
+
+  void pickFiles(mediaXtype) async {
+    result = (await FilePicker.platform.pickFiles(
+      type: FileType.custom,
+      allowedExtensions: mediaXtype,
+      allowMultiple: true,
+      withReadStream: true,
+      withData: true,
+    ))!;
+    if (result == null) {
+      emit(TakeImagess_Error_State());
+    } else {
+      emit(TakeImagess_State());
+    }
+  }
+
+  void uploadImage(Uint8List imageBytes, id) async {
+    Map<String, String> a = {
+      "files": imagee!.path,
+      "ref": "user",
+      "refId": "$id",
+      "field": "intro_logo",
+      "source": "users-permissions"
+    };
+    String url = base_api + "/upload";
+    var stream = http.ByteStream(DelegatingStream.typed(imagee!.openRead()));
+    var uri = Uri.parse(url);
+    int length = imageBytes.length;
+    var request = http.MultipartRequest("POST", uri);
+    var multipartFile = http.MultipartFile('files', stream, length,
+        filename: basename(imagee!.path),
+        contentType: MediaType('image', 'png'));
+    request.files.add(multipartFile);
+    request.fields.addAll(a);
+    var response = await request.send();
+    response.stream.transform(utf8.decoder).listen((value) {
+      print(value);
+    });
+  }
+
+  void uploadImages(id) {
+    result!.files.forEach((element) async {
+      String url = base_api + "/upload";
+      var uri = Uri.parse(url);
+      var request = http.MultipartRequest("POST", uri);
+      Map<String, String> a = {
+        "files": element.path!,
+        "ref": "user",
+        "refId": "$id",
+        "field": "intro_img",
+        "source": "users-permissions"
+      };
+      var stream = http.ByteStream(DelegatingStream.typed(element.readStream!));
+      int? length = element.bytes!.length;
+      var multipartFile = http.MultipartFile('files', stream, length,
+          filename: basename(element.path!), contentType: mediaType);
+      request.fields.addAll(a);
+      request.files.add(multipartFile);
+      final response = await request.send();
+      response.stream.transform(utf8.decoder).listen((value) {
+        print(value);
+        // ignore: void_checks
+      });
+    });
+  }
+
+  LoginModel? loginModel;
+  late var myid;
+  final _auth = FirebaseAuth.instance;
+  late UserCredential authres;
+  register(
+      {String? username,
+      email,
+      password,
+      phone,
+      String? listImages,
+      address,
+      about}) async {
+    emit(cons_Loading_Register());
+    late var response;
+    final url = Uri.parse("$base_api/auth/local/register");
+    Map<String, String> headrs = {'Accept': 'application/json'};
+    Map<String, dynamic> body = {
+      'username': username,
+      'email': email,
+      'password': password,
+      "phone": phone,
+      "city": mycity,
+      "address": address,
+      "about": about,
+      "type_introducer": myType,
+      "Confirmed": false.toString(),
+      "intro_img": listImages,
+      //"intro_logo":imagee.path,
+      "categories": cat_id.toString(),
+      "specailst": spec_id.toString(),
+    };
+    try {
+      authres = await _auth.createUserWithEmailAndPassword(
+          email: email, password: password);
+      await FirebaseFirestore.instance
+          .collection("users")
+          .doc(authres.user!.uid)
+          .set({
+        'username': username,
+        'email': email,
+        "phone": phone,
+        "userid": authres.user!.uid,
+      }).then((value) async {
+        response = await http.post(url, headers: headrs, body: body);
+        if (response.statusCode == 200) {
+          var jdson = jsonDecode(response.body);
+          final loadeddata = jdson['user'];
+          myid = loadeddata["id"];
+          print(response.body);
+
+          ///upload image logo kant adema h3mlha fe profile
+          // uploadImage(imagee.readAsBytesSync(),myid);
+          uploadImages(myid);
+          emit(cons_Register_Scusess());
+          return true;
+        } else if (response.statusCode == 400) {
+          var jdsonn = jsonDecode(response.body);
+          loginModel = LoginModel.fromJson(jdsonn);
+          print(loginModel?.message!
+              .map((e) => e.messages!.map((e) => e.message.toString())));
+          emit(cons_Login_Error(loginModel!));
+          return false;
+        } else {
+          emit(cons_Register_final_Error());
+        }
+      });
+    } on FirebaseException catch (y) {
+      emit(cons_Register_firebase_Error());
+      throw y;
+    } catch (e) {
+      emit(cons_Register_finaly_Error());
+    }
+  }
+
+  String? tokenuser;
+  late String myuserId;
+  login(String email, String password) async {
+    emit(cons_Loading_login());
+    final url = Uri.parse("$base_api/auth/local");
+    Map<String, String> headrs = {
+      'Accept': 'application/json',
+    };
+    Map<String, String> body = {
+      'identifier': email,
+      'password': password,
+    };
+    var response = await http.post(url, headers: headrs, body: body);
+    if (response.statusCode == 200) {
+      var jdson = jsonDecode(response.body);
+      loginModel = LoginModel.fromJson(jdson);
+      tokenuser = loginModel?.token;
+      CashHelper.putData("user_token", tokenuser);
+      print(loginModel?.userClass!.email);
+      print(loginModel?.token);
+      print(body);
+
+      emit(cons_Login_Scusess(loginModel!));
+      return true;
+    } else if (response.statusCode == 400) {
+      var jdsonn = jsonDecode(response.body);
+      loginModel = LoginModel.fromJson(jdsonn);
+      emit(cons_Login_Error(loginModel!));
+      // ignore: avoid_print
+      print(loginModel?.message!
+          .map((e) => e.messages!.map((e) => e.message.toString())));
+    } else if (response.persistentConnection) {
+      emit(cons_not_connected_net_login());
+    }
+  }
+
+  //////////////////////////////////////////////ForgetPass////////////
+
+  Future<http.Response> updatePassStrapi(String newPassword, int? id) {
+    return http.put(
+      Uri.parse("$base_api/Users/$id"),
+      headers: <String, String>{'Content-Type': 'application/json'},
+      body: jsonEncode(<String, String>{
+        'password': newPassword,
+      }),
+    );
+  }
+
+  Future<http.Response> updateForget(String table, int id) {
+    return http.put(
+      Uri.parse("$base_api/$table/$id"),
+      headers: <String, String>{'Content-Type': 'application/json'},
+      body: jsonEncode(<String, bool>{'forgetpass': true}),
+    );
+  }
+
+  late String error = "Email IsNot Exist";
+
+  Future<void> getUser(table, email) async {
+    final url = Uri.parse("$base_api/$table?_where[email]=$email");
+    final http.Response res = await http.get(url);
+    if (res.statusCode == 200) {
+      late int myid;
+      String? myemail;
+      print(res.body.toString());
+      var user = jsonDecode(res.body);
+      for (var x in user) {
+        myid = x['id'];
+        myemail = x['email'];
+      }
+      if (myemail == email) {
+        FirebaseAuth.instance
+            .sendPasswordResetEmail(email: email)
+            .then((value) {
+          updateForget(table, myid);
+          emit(LoginChangePassSucessState());
+        }).catchError((onError) {
+          emit(LoginChangePassSucessState());
+        });
+      } else {
+        return myToast(message: error);
+      }
+    } else {
+      print('no connect');
+    }
+  }
+
+  ////////////Add Post ////////////////
+  ///
+  PostModel? postModel;
+
+  Future<PostModel?> AddPost(String content, String time, dynamic id) async {
+    emit(ConsAddPostUserLoadingState());
+    final response = await http.post(
+      Uri.parse("$base_api/Posts?_where[users_id]=$id"),
+      headers: <String, String>{
+        'Content-Type': 'application/json ',
+      },
+      body: jsonEncode(<dynamic, dynamic>{
+        'content': content,
+        'time': time,
+        'users_id': 336,
+      }),
+    );
+    if (response.statusCode == 200) {
+      postModel = PostModel.fromJson(jsonDecode(response.body));
+      var jdson = jsonDecode(response.body);
+      var id_question = jdson['id'];
+      print(" id is $id_question");
+      if (imagee == null) {
+        print("arf");
+      } else {
+        uploadPostImage(imagee!.readAsBytesSync(), id_question);
+      }
+
+      emit(ConsAddPostUserSucessState());
+      return postModel;
+    } else if (response.statusCode == 500) {
+      var x = response.body.toString();
+    } else {
+      print(response.body.toString());
+      emit(ConsAddPostUserErrorState());
+      throw Exception('Failed to Add Post');
+    }
+  }
+
+  /////////uploadImagePost/////////////////
+  void uploadPostImage(Uint8List imageBytes, id) async {
+    Map<String, String> a = {
+      "files": imagee!.path,
+      "ref": "posts",
+      "refId": "$id",
+      "field": "img_post",
+    };
+    String url = base_api + "/upload";
+    var stream = http.ByteStream(DelegatingStream.typed(imagee!.openRead()));
+    var uri = Uri.parse(url);
+    int length = imageBytes.length;
+    var request = http.MultipartRequest("POST", uri);
+    var multipartFile = http.MultipartFile('files', stream, length,
+        filename: basename(pickedFile.path),
+        contentType: MediaType('image', 'jpg'));
+    request.files.add(multipartFile);
+    request.fields.addAll(a);
+    var response = await request.send();
+    response.stream.transform(utf8.decoder).listen((value) {
+      print(value);
+    });
+  }
+}
---
+++ siham-master/lib/modules/User/login/screren/login_user.dart
@@ -0,0 +1,225 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_fadein/flutter_fadein.dart';
+import 'package:flutter_spinkit/flutter_spinkit.dart';
+
+import 'package:helpy_app/modules/User/cubit/cubit.dart';
+import 'package:helpy_app/modules/User/cubit/states.dart';
+
+import 'package:helpy_app/modules/User/login/main_login.dart';
+import 'package:helpy_app/modules/User/main.dart';
+import 'package:helpy_app/modules/User/post/add_post.dart';
+import 'package:helpy_app/modules/otp/otp_register.dart';
+import 'package:helpy_app/shared/compononet/custom_dialog.dart';
+import 'package:helpy_app/shared/componotents.dart';
+import 'package:helpy_app/shared/localization/translate.dart';
+import 'package:helpy_app/shared/my_colors.dart';
+
+class LoginIntro extends StatefulWidget {
+  @override
+  State<LoginIntro> createState() => _LoginIntroState();
+}
+
+class _LoginIntroState extends State<LoginIntro> {
+  var emailsController = TextEditingController();
+  var passsController = TextEditingController();
+  GlobalKey<FormState> formState = GlobalKey();
+  GlobalKey<FormState> formState2 = GlobalKey();
+  bool myState = false;
+  var newPasswordController = TextEditingController();
+  var emailController = TextEditingController();
+
+  @override
+  void initState() {
+    super.initState();
+    emailsController = TextEditingController();
+    passsController = TextEditingController();
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+    emailsController.dispose();
+    passsController.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return BlocListener<UserCubit, cons_login_Register_States>(
+      listener: (ctx, state) {
+        if (state is cons_Loading_login) {
+          myState = true;
+        } else if (state is cons_Login_Scusess) {
+          if (state.loginModel.userClass!.forgetpass == true) {
+            myToast(message: "change pass");
+            showDialog(
+                context: context,
+                builder: (ctx) => CustomTextFieldDialog(
+                    controller: newPasswordController,
+                    title: "EnterPass",
+                    buttonText: "Send",
+                    hintText: "Password",
+                    onPressed: () {
+                      FocusScope.of(context).unfocus();
+                      UserCubit.get(context).updatePassStrapi(
+                          newPasswordController.text,
+                          state.loginModel.userClass!.id);
+                      navigateTo(context, LoginIntro());
+                    }));
+            myToast(
+              message: mytranslate(context, "sucesspass"),
+            );
+
+            ///dialog yd5l password then y3ml update le pasword bta3 strapi
+          } else if (state.loginModel.userClass!.confirmed == true) {
+            myToast(message: "Done login");
+            navigateTo(context, CreatePost());
+          } else {
+            My_CustomAlertDialog(
+              icon: Icons.warning_rounded,
+              bigTitle: mytranslate(context, "dialogRegistertitle"),
+              content: mytranslate(context, "gialogLogin"),
+              context: context,
+              pressColor: myAmber,
+              pressTitle: mytranslate(context, "done"),
+              pressText: () {
+                Navigator.of(context).pop();
+              },
+            );
+          }
+          navigateTo(context, UserMain());
+          myState = false;
+        } else if (state is cons_Login_Error) {
+          state.loginModel.message!
+              .map((e) => e.messages!.map((e) => My_CustomAlertDialog(
+                    icon: Icons.warning_rounded,
+                    bigTitle: mytranslate(context, "dialogRegistertitle"),
+                    content: e.message.toString(),
+                    context: context,
+                    pressColor: myAmber,
+                    pressTitle: mytranslate(context, "done"),
+                    pressText: () {
+                      Navigator.of(context).pop();
+                    },
+                  )))
+              .toString();
+          myState = false;
+        } else if (state is LoginChangePassSucessState) {
+          myToast(message: (mytranslate(context, "Emailissent")));
+        }
+      },
+      child: FadeIn(
+        duration: const Duration(seconds: 1),
+        curve: Curves.easeInCirc,
+        child: Form(
+          key: formState,
+          child: Column(
+            children: [
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 25.00),
+                child: My_TextFormFiled(
+                  controller: emailsController,
+                  myhintText: mytranslate(context, "email"),
+                  validator: (String? s) {
+                    if (s!.isEmpty) return "Email is required";
+                  },
+                ),
+              ),
+              const SizedBox(
+                height: 15,
+              ),
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 25.00),
+                child: My_TextFormFiled(
+                  controller: passsController,
+                  myhintText: mytranslate(context, "Password"),
+                  validator: (String? s) {
+                    if (s!.isEmpty) return "Password is required";
+                  },
+                ),
+              ),
+              const SizedBox(
+                height: 15,
+              ),
+              Center(
+                  child: Mybutton(
+                      color: myAmber,
+                      context: context,
+                      onPress: () async {
+                        if (formState.currentState != null &&
+                            formState.currentState!.validate()) {
+                          FocusScope.of(context).unfocus();
+                          await UserCubit.get(context).login(
+                              emailsController.text, passsController.text);
+                        }
+                      },
+                      title: myState
+                          ? const Center(
+                              child: SpinKitCircle(
+                              color: Colors.white,
+                            ))
+                          : Text(
+                              mytranslate(context, "login"),
+                              style: const TextStyle(
+                                  color: Colors.white, fontSize: 18),
+                            ))),
+              const SizedBox(
+                height: 20,
+              ),
+              Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Text("${mytranslate(context, "donthave")} - ",
+                          textAlign: TextAlign.center,
+                          style: const TextStyle(
+                            color: Colors.black,
+                          )),
+                      InkWell(
+                        onTap: () {
+                          FocusScope.of(context).unfocus();
+                          navigateTo(context, OtpRegister(false));
+                        },
+                        child: Text(
+                          mytranslate(context, "Signup"),
+                          style: const TextStyle(
+                              fontSize: 14,
+                              color: Colors.black,
+                              fontWeight: FontWeight.bold),
+                        ),
+                      ),
+                    ],
+                  ),
+                  TextButton(
+                    onPressed: () {
+                      FocusScope.of(context).unfocus();
+                      showDialog(
+                          context: context,
+                          builder: (ctx) => CustomTextFieldDialog(
+                              controller: emailController,
+                              title: "EnterEmail",
+                              buttonText: "Send",
+                              hintText: "email",
+                              onPressed: () {
+                                Navigator.pop(context);
+                                UserCubit.get(context)
+                                    .getUser("users", emailController.text);
+                                navigateTo(context, Main_login());
+                              }));
+                    },
+                    child: Text(
+                      mytranslate(context, "forgetpass"),
+                      style: TextStyle(color: myAmber),
+                    ),
+                  )
+                ],
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
